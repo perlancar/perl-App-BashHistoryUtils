91277e47c8a07df19ff8b716d9cc92eabf11ad5e
perlancar (@pc-office) <perlancar@gmail.com>
2015-11-04 21:14:18 +0700

    Add utility: grep-bash-history-entries, add dry_run for delete

--- lib/App/BashHistoryUtils.pm
+++ lib/App/BashHistoryUtils.pm
@@ -14,36 +14,47 @@ our %arg_histfile = (
         schema => 'str*',
         default => ($ENV{HISTFILE} // "$ENV{HOME}/.bash_history"),
         cmdline_aliases => {f=>{}},
+        'x.completion' => ['filename'],
     },
 );
 
-$SPEC{delete_entries} = {
-    v => 1.1,
-    summary => 'Delete entries from bash history file',
-    args => {
-        %arg_histfile,
-        pattern => {
-            summary => 'Delete entries matching a regex pattern',
-            schema => 're*',
-            cmdline_aliases => {p=>{}},
-        },
-        ignore_case => {
-            schema => ['bool', is=>1],
-            cmdline_aliases => {i=>{}},
-        },
-        max_age => {
-            summary => 'Delete entries older than ',
-            schema => 'duration*',
-            'x.perl.coerce_to' => 'int(secs)',
-        },
+our %args_filtering = (
+    pattern => {
+        summary => 'Match entries using a regex pattern',
+        schema => 're*',
+        cmdline_aliases => {p=>{}},
+        tags => ['category:filtering'],
+        pos => 0,
     },
-};
-sub delete_entries {
+    max_age => {
+        summary => 'Match entries older than a certain age',
+        schema => 'duration*',
+        'x.perl.coerce_to' => 'int(secs)',
+        tags => ['category:filtering'],
+    },
+    min_age => {
+        summary => 'Match entries younger than a certain age',
+        schema => 'duration*',
+        'x.perl.coerce_to' => 'int(secs)',
+        tags => ['category:filtering'],
+    },
+    ignore_case => {
+        schema => ['bool', is=>1],
+        cmdline_aliases => {i=>{}},
+    },
+    invert_match => {
+        schema => ['bool', is=>1],
+        #cmdline_aliases => {v=>{}}, # clashes with --version
+    },
+);
+
+sub _do {
     require Bash::History::Read;
     require Capture::Tiny;
     require Cwd;
     #require File::Temp;
 
+    my $which = shift;
     my %args = @_;
 
     my $histfile = $args{histfile};
@@ -70,26 +81,69 @@ sub delete_entries {
                             $main::TS < $now-$args{max_age}) {
                         $main::PRINT = 0;
                     }
+                    if (defined($args{min_age}) &&
+                            $main::TS > $now-$args{min_age}) {
+                        $main::PRINT = 0;
+                    }
                     if ($pat && $_ =~ $pat) {
                         $main::PRINT = 0;
+                    }
+
+                    if ($which eq 'grep') {
+                        $main::PRINT = !$main::PRINT;
+                    }
+                    if ($args{invert_match}) {
+                        $main::PRINT = !$main::PRINT;
                     }
                 });
         });
 
-    my $tempfile = "$realhistfile.tmp.$$";
-    open my($fh), ">", $tempfile
-        or return [500, "Can't open temporary file '$tempfile': $!"];
+    if ($which eq 'grep' ||
+            $which eq 'delete' && $args{-dry_run}) {
+        return [200,"OK", $stdout, {'cmdline.skip_format'=>1}];
+    } elsif ($which eq 'delete') {
+        my $tempfile = "$realhistfile.tmp.$$";
+        open my($fh), ">", $tempfile
+            or return [500, "Can't open temporary file '$tempfile': $!"];
 
-    print $fh $stdout
-        or return [500, "Can't write (1) to temporary file '$tempfile': $!"];
+        print $fh $stdout
+            or return [500, "Can't write (1) to temporary file '$tempfile': $!"];
 
-    close $fh
-        or return [500, "Can't write (2) to temporary file '$tempfile': $!"];
+        close $fh
+            or return [500, "Can't write (2) to temporary file '$tempfile': $!"];
 
-    rename $tempfile, $realhistfile
-        or return [500, "Can't replace temporary file '$tempfile' to '$realhistfile': $!"];
+        rename $tempfile, $realhistfile
+            or return [500, "Can't replace temporary file '$tempfile' to '$realhistfile': $!"];
+    }
 
     [200,"OK"];
+}
+
+$SPEC{grep_bash_history_entries} = {
+    v => 1.1,
+    summary => 'Show matching entries from bash history file',
+    args => {
+        %arg_histfile,
+        %args_filtering,
+    },
+};
+sub grep_bash_history_entries {
+    _do('grep', @_);
+}
+
+$SPEC{delete_bash_history_entries} = {
+    v => 1.1,
+    summary => 'Delete matching entries from bash history file',
+    args => {
+        %arg_histfile,
+        %args_filtering,
+    },
+    features => {
+        dry_run => 1,
+    },
+};
+sub delete_bash_history_entries {
+    _do('delete', @_);
 }
 
 1;
